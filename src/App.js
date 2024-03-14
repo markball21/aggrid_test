12d765a34cfe1f9c69eda0ed9b387a31f26b7b67
markball21 <mark@bocksolutions.com>
2024-03-13 21:16:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ const App = () => {
         const data = await response.json();
         console.log('Fetched data:', data); // Add console output
         setRowData(data);
-        const colDefs = gridOptions.api.getColumnDefs();
-        colDefs.length = 0;
+        // const colDefs = gridOptions.api.getColumnDefs();
+        // colDefs.length = 0;
         const keys = Object.keys(data[0]);
         keys.forEach(key => colDefs.push({ field: key }));
         gridOptions.api.setColumnDefs(colDefs);
