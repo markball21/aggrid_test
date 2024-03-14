a1843ba7e557ad7b8b785c1db78b27fef2b8d764
markball21 <mark@bocksolutions.com>
2024-03-13 22:46:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { exportDataAsCsv } from 'ag-grid-community';
 
 const App = () => {
   const [rowData, setRowData] = useState([]);
   const [columnDefs, setColumnDefs] = useState([]);
+  const gridApiRef = useRef(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,7 +19,7 @@ const App = () => {
         setRowData(data);
         const keys = Object.keys(data[0]);
         console.log('Keys:', keys);
-        const newColumnDefs = keys.map(key => ({ headerName: key.toUpperCase(), field: key, sortable: true, filter: true }));
+        const newColumnDefs = keys.map(key => ({ headerName: key.toUpperCase(), field: key, sortable: true, filter: true, resizable: true, movable: true }));
         setColumnDefs(newColumnDefs);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -27,14 +29,28 @@ const App = () => {
     fetchData();
   }, []);
 
+  const onGridReady = (params) => {
+    gridApiRef.current = params.api;
+  };
+
+  const exportGridData = () => {
+    if (gridApiRef.current) {
+      gridApiRef.current.exportDataAsCsv();
+    }
+  };
+
   console.log('rowData:', rowData);
   console.log('columnDefs:', columnDefs);
 
   return (
     <div className="ag-theme-alpine" style={{ height: 800, width: 1200 }}>
+      <div>
+        <button onClick={exportGridData}>Export</button>
+      </div>
       <AgGridReact
         rowData={rowData}
         columnDefs={columnDefs}
+        onGridReady={onGridReady}
       />
     </div>
   );
